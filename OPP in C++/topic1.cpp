f3207c8bc9a8ec9ffd1f9b3e4892acca6105f642
Mystic <nicholelin3929@gmail.com>
2024-01-02 13:58:15 -0500

    Classes, Access Modifiers, Constructors

--- OPP in C++/topic1.cpp
+++ OPP in C++/topic1.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
-int main(){
+class Friend{
+public:
+    //Below: these are attributes
+    string Name;
+    int Age;
+    string BirthMonth;
 
-    return 0;
-}
+    void Intro(){ //Behavior
+        cout<<"My name is "<< Name<<endl;
+        cout<<"I am "<<Age<<" years old"<<endl;
+        cout<<"My birth month is: "<<BirthMonth<<endl; 
+        cout<<endl;
+    }
+
+    //Constructors
+    Friend(string name, int age, string birthMonth){
+        Name = name;
+        Age = age;
+        BirthMonth = birthMonth;
+    }
+};
+int main(){
+    Friend Friend1("Shao",19,"December");
+    /* Can get rid of it since we have it inside our constructor
+    Friend1.Name = "Shao";
+    Friend1.Age = 19;
+    Friend1.BirthMonth = "December";*/
+    Friend1.Intro();
+
+    Friend Friend2("Tracy", 16, "Feburary");
+    Friend2.Intro();
+    
